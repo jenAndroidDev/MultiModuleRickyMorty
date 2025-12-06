4ecf8b093aed54a65cbefc53df01175b9a0895d2
Jenin Joseph <104613017+jenAndroidDev@users.noreply.github.com>
2025-12-06 18:16:51 +0530

    Paging Utilities Added For Loadstates

--- core/common/src/main/kotlin/com/rmworld/core/common/paging/LoadStates.kt
+++ core/common/src/main/kotlin/com/rmworld/core/common/paging/LoadStates.kt
@@ -1,2 +1,51 @@
-package com.rmworld.core.common.paging 
+package com.rmworld.core.common.paging
 
+import com.rmworld.core.common.paging.LoadType
+
+public data class LoadStates(
+    public val refresh: LoadState,
+    public val prepend: LoadState,
+    public val append: LoadState,
+    public val action: LoadState
+) {
+
+    public inline fun forEach(op: (LoadType, LoadState) -> Unit) {
+        op(LoadType.REFRESH, refresh)
+        op(LoadType.PREPEND, prepend)
+        op(LoadType.APPEND, append)
+        op(LoadType.ACTION, action)
+    }
+
+    internal fun modifyState(loadType: LoadType, newState: LoadState): LoadStates {
+        return when (loadType) {
+            LoadType.REFRESH -> copy(
+                refresh = newState
+            )
+            LoadType.PREPEND -> copy(
+                prepend = newState
+            )//jeni
+            LoadType.APPEND -> copy(
+                append = newState
+            )
+            LoadType.ACTION -> copy(
+                action = newState
+            )
+        }
+    }
+
+    internal fun get(loadType: LoadType) = when (loadType) {
+        LoadType.REFRESH -> refresh
+        LoadType.PREPEND -> prepend
+        LoadType.APPEND -> append
+        LoadType.ACTION -> action
+    }
+
+    internal companion object {
+        val IDLE = LoadStates(
+            refresh = LoadState.NotLoading.InComplete,
+            prepend = LoadState.NotLoading.InComplete,
+            append = LoadState.NotLoading.InComplete,
+            action = LoadState.NotLoading.InComplete
+        )
+    }
+}
--- core/common/src/main/kotlin/com/rmworld/core/common/paging/PagedData.kt
+++ core/common/src/main/kotlin/com/rmworld/core/common/paging/PagedData.kt
@@ -1,2 +1,8 @@
-package com.rmworld.core.common.paging 
+package com.rmworld.core.common.paging
 
+data class PagedData<T>(
+    val data: List<T>,
+    val totalCount: Int,
+    val prevKey: String?,
+    val nextKey: String?
+)
