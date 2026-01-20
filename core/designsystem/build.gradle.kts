f1ddcab1b659ac89d9ea897f9cd3fd64d9e072ac
Jenin Joseph <104613017+jenAndroidDev@users.noreply.github.com>
2026-01-20 15:03:13 +0530

    Navigation Rail Added For Adaptive Layouts.

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -10,4 +10,7 @@ dependencies{
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation)
+    api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.material3.adaptive)
+    api(libs.androidx.compose.material3.navigationSuite)
 }
