7fe40ddcbea790e78631a4f32b31a91b5674254b
Jenin Joseph <104613017+jenAndroidDev@users.noreply.github.com>
2026-01-22 16:13:52 +0530

    techdebt:remove custom dispatchers.

---
+++ core/network/src/main/kotlin/com/rmworld/core/network/di/DispatchersModule.kt
@@ -0,0 +1,24 @@
+package com.rmworld.core.network.di
+
+import com.rmworld.core.network.Dispatcher
+import com.rmworld.core.network.RMWorldDispatchers
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatchersModule {
+
+    @Provides
+    @Dispatcher(RMWorldDispatchers.IO)
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @Dispatcher(RMWorldDispatchers.Default)
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+}
